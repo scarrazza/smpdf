bdfaeab45f83d0f3bb8e418bc81c31904e40dcf1
Zahari <zaharid@gmail.com>
2015-05-04 23:56:26 +0200

    Add actions.py
    This file should have been part of the previous commit.

---
+++ actions.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon May  4 18:58:08 2015
+
+@author: zah
+"""
+import os.path as  osp
+import re
+
+import pandas as pd
+
+import smpdflib as lib
+
+
+#TODO: Specify base
+def save_violins(results, base_pdf, output_dir):
+    for obs, fig in lib.compare_violins(results, base_pdf = results[0].pdf):
+        fig.savefig(osp.join(output_dir, "figures", '%s.pdf' % obs))
+
+def save_as(results, output_dir):
+    if not isinstance(results, pd.DataFrame):
+        results = lib.summed_results_table(results)
+    for (process, nf), fig in lib.plot_alphaS(results):
+        name = 'alpha_plot_%s(nf_%d).pdf'%(process,nf)
+        name = re.sub(r'[^\w\.]', '_', name)
+        fig.savefig(osp.join(output_dir, "figures", name))
