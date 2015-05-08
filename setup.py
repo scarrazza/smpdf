9078218f167cbe4a1db44a1bf687afd24e438771
Zahari <zaharid@gmail.com>
2015-05-08 10:26:13 +0200

    Start code reorganization
    The code should work as a pythn module, installed with setuptools.
    
    This is not yet functional.

--- setup.py
+++ setup.py
@@ -1,9 +1,16 @@
-from setuptools import setup, Extension
+from __future__ import print_function
+import sys
+from setuptools import setup, Extension, find_packages
 import subprocess
 
 def call_command(command):
     l = command.split()
-    result = subprocess.check_output(l)
+    try:
+        result = subprocess.check_output(l)
+    except OSError as e:
+        print("Could not call %s: %s.\n"
+              "Please make sure the relevant command is isntalled."
+              % (l[0], e.strerror), file=sys.stderr)
     return result.decode().rstrip()
 
 lhapdf_includes = call_command('lhapdf-config --cflags').split()
@@ -16,17 +23,28 @@ module1 = Extension('applwrap',
                                           applgrid_libs + lhapdf_libs),
                     extra_link_args = (lhapdf_includes + applgrid_includes  +
                                           applgrid_libs + lhapdf_libs),
-                    sources = ['applwrap/applwrap.cc'], language="c++")
+                    sources = ['src/applwrap/applwrap.cc'], language="c++")
 
-setup (name = 'PackageName',
-       version = '1.0',
-       description = 'This is a demo package',
-       author = 'Martin v. Loewis',
-       author_email = 'martin@v.loewis.de',
-       url = 'https://docs.python.org/extending/building',
-       long_description = '''
-This is really just a demo package.
-''',
+setup (name = 'smpdf',
+       version = '0.1',
+       description = "PDFs for phenomenology",
+       author = 'Stefano Carrazza and Zahari Kassabov',
+       author_email = 'kassabov@to.infn.it',
+       url = 'https://github.com/scarrazza/smpdf',
+       long_description = "See `smpdf --help` for the full list of options",
        ext_modules = [module1],
-       scripts = ['smpdf.py']
+       scripts = ['scripts/smpdf.py'],
+       package_dir = {'': 'src'},
+       packages = find_packages('src'),
+       classifiers=[
+            'License :: OSI Approved :: BSD License',
+            'Operating System :: OS Independent',
+            'Programming Language :: Python',
+            'Topic :: Scientific/Engineering',
+            'Topic :: Scientific/Engineering :: Physics',
+            'Programming Language :: Python :: 2',
+            'Programming Language :: Python :: 2.7',
+            'Programming Language :: Python :: 3',
+            'Programming Language :: Python :: 3.4',
+            ],
        )
