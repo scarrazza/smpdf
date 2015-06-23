9eb1b31942f70474d3676a3ca22ef5b9f9ac39fd
Zahari <zaharid@gmail.com>
2015-06-23 14:48:00 +0200

    Bin plotting example

--- smallthing.py
+++ smallthing.py
@@ -1,13 +1,28 @@
+import os.path as osp
 import shelve
 
-import numpy as np
+from smpdflib.core import make_observable, PDF, produce_results, results_table
 
-from smpdflib.core import make_observable, PDF, produce_results
+from smpdflib.plots import plot_bindist
+
+import matplotlib.pyplot as plt
+
+import smpdflib.core as lib
+
+
+libpath = osp.dirname(lib.__file__)
+stylefilename = osp.join(libpath, 'main.mplstyle')
+plt.style.use(stylefilename)
 
 db = shelve.open("db/db")
 obs = make_observable("data/higgs/ggH_pt_13tev.root", order=1)
 pdf = PDF("1000rep")
-result = produce_results([pdf], [obs], db=db)[0]
-ratios = result._all_vals.mean(axis=1)/result[0]
-print(ratios)
+pdf2 = PDF('new-120-abs')
+results = produce_results([pdf, pdf2], [obs], db=db)
 
+obs_table = results_table(results)
+
+for (obs,b), fig in plot_bindist(obs_table, 6, base_pdf = pdf):
+    path = "%s_bin_%s.pdf" % (obs, b+1)
+    fig.savefig(path)
+    plt.close(fig)
