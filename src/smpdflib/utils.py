ea33e1d7ca497f9af02d084082f08db862e305aa
Zahari <zaharid@gmail.com>
2015-06-24 18:14:34 +0200

    Add try..finally block to supress_stdout cm

--- src/smpdflib/utils.py
+++ src/smpdflib/utils.py
@@ -23,7 +23,9 @@ def supress_stdout():
         saved_stdout_fd = os.dup(stdout_flieno)
         os.dup2(devnull_fileno, stdout_flieno)
 
-    with redirect_stdout(devnull):
-        yield
-    if redirect:
-        os.dup2(stdout_flieno, saved_stdout_fd)
+    try:
+        with redirect_stdout(devnull):
+            yield
+    finally:
+        if redirect:
+            os.dup2(stdout_flieno, saved_stdout_fd)
