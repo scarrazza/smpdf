059b39d5ed1bbc665b2b35e87ddb04053f009b4f
Zahari <zaharid@gmail.com>
2015-08-04 15:05:41 +0200

    Complete threshold study script

--- extrascripts/thresholds.py
+++ extrascripts/thresholds.py
@@ -6,10 +6,14 @@ Created on Mon Aug  3 16:29:18 2015
 """
 import shelve
 import logging
+import glob
+import itertools
+import contextlib
 
 from smpdflib.initialization import init_app
 if __name__ == '__main__':
-    init_app()
+    with contextlib.suppress(RuntimeError):
+        init_app()
 
 import matplotlib.pyplot as plt
 
@@ -17,20 +21,31 @@ from smpdflib.core import (PDF, make_observable, produce_results,
                            get_smpdf_lincomb)
 
 
-pdf = PDF("1000rep")
-obs = make_observable("data/higgs/ggh_13tev.root", order='NLO')
+pdf = PDF("MC900_nnlo")
+obs = [make_observable(path, order='NLO') for path in itertools.chain(
+       glob.glob("data/z/*.root"),
+       glob.glob("data/w/*.root"),
+       glob.glob("data/higgs/*.root"),
+       glob.glob("data/ttbar/*.root"),
+       )]
+
+
 
 tolerance = 0.05
 
-thresholds = [0.0, 0.25, 0.5, 0.75, 0.9]
+thresholds = [0.0, 0.25, 0.5, 0.75, 0.9, 0.99]
 
 if __name__ == '__main__':
-    logging.basicConfig(level=logging.INFO)
+    logging.basicConfig(level=logging.WARNING)
     db = shelve.open('db/db')
-    results = produce_results([pdf], [obs],  db=db)
+
+    try:
+        results = produce_results([pdf], obs,  db=db)
+    finally:
+        db.close()
     for t in thresholds:
         V, desc = get_smpdf_lincomb(pdf, results, target_error=tolerance,
                           correlation_threshold=t)
         #Sprint(V.shape)
+        print("For thresholf %.2f we get:" %t)
         print(desc)
-    db.close()
