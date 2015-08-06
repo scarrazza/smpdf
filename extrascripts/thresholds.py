c3f8e4cfcaf0a9826801707ef1fa37827ee1425d
Zahari <zaharid@gmail.com>
2015-08-06 09:29:05 +0200

    Final threshold script

--- extrascripts/thresholds.py
+++ extrascripts/thresholds.py
@@ -4,6 +4,7 @@ Created on Mon Aug  3 16:29:18 2015
 
 @author: zah
 """
+import json
 import shelve
 import logging
 import glob
@@ -23,10 +24,10 @@ from smpdflib.core import (PDF, make_observable, produce_results,
 
 pdf = PDF("MC900_nnlo")
 obs = [make_observable(path, order='NLO') for path in itertools.chain(
-       glob.glob("data/z/*.root"),
-       glob.glob("data/w/*.root"),
-       glob.glob("data/higgs/*.root"),
-       glob.glob("data/ttbar/*.root"),
+ #      sorted(glob.glob("data/z/*.root")),
+       sorted(glob.glob("data/w/*.root")),
+#       sorted(glob.glob("data/higgs/*.root")),
+#       sorted(glob.glob("data/ttbar/*.root")),
        )]
 
 
@@ -36,6 +37,7 @@ tolerance = 0.05
 thresholds = [0.0, 0.25, 0.5, 0.75, 0.9, 0.99]
 
 if __name__ == '__main__':
+    neig = []
     logging.basicConfig(level=logging.WARNING)
     db = shelve.open('db/db')
 
@@ -46,6 +48,8 @@ if __name__ == '__main__':
     for t in thresholds:
         V, desc = get_smpdf_lincomb(pdf, results, target_error=tolerance,
                           correlation_threshold=t)
-        #Sprint(V.shape)
+        neig.append(V.shape[1])
         print("For thresholf %.2f we get:" %t)
         print(desc)
+    with open("thresholds.json", 'w') as f:
+        json.dump([thresholds, neig], f)
