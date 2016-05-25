91f595402b095c1dec5c12a3d3e5a216b713e4f1
Stefano Carrazza <stefano.carrazza@cern.ch>
2016-05-25 18:56:02 +0200

    updating version to 1.1

--- setup.py
+++ setup.py
@@ -37,7 +37,7 @@ module1 = Extension('applwrap',
                     sources = ['src/applwrap/applwrap.cc'], language="c++")
 
 setup (name = 'smpdf',
-       version = '1.0',
+       version = '1.1',
        description = "PDFs for phenomenology",
        author = 'Stefano Carrazza and Zahari Kassabov',
        author_email = 'kassabov@to.infn.it',
